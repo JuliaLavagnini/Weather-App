a4d1e073af561967d497000453d56da3740ffe95
Julia Lavagnini Maia <julia.lavagninimaia@gmail.com>
2023-06-11 13:15:34 +0100

    updated the media content on JS file

--- src/app.js
+++ src/app.js
@@ -1,50 +1,78 @@
-function formatDay(timestamp) {
+function iconNameFunction(iconName) {
+  switch (iconName) {
+    case "01d":
+      return "media/sunny.png";
+    case "01n":
+      return "media/full-moon.png";
+    case "02d":
+      return "media/cloudy-day.png";
+    case "02n":
+      return "media/cloudy-night.png";
+    case "03d":
+      return "media/cloudy.png";
+    case "03n":
+      return "media/cloudy.png";
+    case "04d":
+      return "media/clouds.png";
+    case "04n":
+      return "media/clouds.png";
+    case "09d":
+      return "media/drizzle.png";
+    case "10d":
+      return "media/rain.png";
+    case "11d":
+      return "media/lightning-bolt.png";
+    case "013d":
+      return "media/snowing.png";
+    default:
+      return "media/fog.png";
+  }
+}
+
+function daysName(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
   return days[day];
 }
 
 function displayForecast(response) {
   let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    let iconName = forecastDay.weather[0].icon;
+    if (index < 5) {
       forecastHTML =
         forecastHTML +
-        `<div class="col">
-            <img
-              src="https://cdn-icons-gif.flaticon.com/10973/10973658.gif${
-                forecastDay.weather[0].icon
-              }"      
-              alt=""    
-              width="40px"    
-              id="forecast-icon"    
-            />        
-            <p>     
-              <span id="forecast-min"> ${Math.round(
-                forecastDay.temp.max
-              )}°</span> /
-              <span id="forecast-max">${Math.round(
-                forecastDay.temp.min
-              )}°</span>
-            </p>
-              <p class="day">${formatDay(formatDay.dt)}</p>
-            </div>
-        `;
+        `
+      <div class="col">
+        <img
+          src=${iconNameFunction(iconName)}
+          alt="${iconName}"
+          width="42px"
+          id="forecast-icon"
+        />
+        <p>
+          <span id="forecast-min"> ${Math.round(forecastDay.temp.max)}°</span> /
+          <span id="forecast-max">${Math.round(forecastDay.temp.min)}°</span>
+        </p>
+        <p class="day">${daysName(forecastDay.dt)}</p>
+      </div>`;
     }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+/*;*/
 
 function getForecast(coordinates) {
   let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=hourly,daily&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -56,46 +84,9 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity span");
   let windElement = document.querySelector("#wind span");
 
-  console.log(response.data.weather[0].icon);
+  let iconName = response.data.weather[0].icon;
 
-  switch (response.data.weather[0].main) {
-    case "01d":
-      weatherPic.src = "https://cdn-icons-gif.flaticon.com/6455/6455017.gif";
-      break;
-
-    case "01n":
-      weatherPic.src = "https://cdn-icons-gif.flaticon.com/6455/6455031.gif";
-      break;
-  }
-
-  switch (response.data.weather[0].main) {
-    case "Clouds":
-      weatherPic.src = "https://cdn-icons-gif.flaticon.com/6455/6455053.gif";
-      break;
-
-    case "Rain":
-      weatherPic.src = "https://cdn-icons-gif.flaticon.com/6455/6455055.gif";
-      break;
-
-    case "Drizzle":
-      weatherPic.src = "https://cdn-icons-gif.flaticon.com/6455/6455057.gif";
-      break;
-
-    case "Thunderstorm":
-      weatherPic.src = "https://cdn-icons-gif.flaticon.com/6455/6455012.gif";
-      break;
-
-    case "Snow":
-      weatherPic.src = "https://cdn-icons-gif.flaticon.com/6454/6454998.gif";
-      break;
-
-    case "Mist":
-      weatherPic.src = "https://cdn-icons-gif.flaticon.com/6454/6454995.gif";
-      break;
-
-    default:
-      weatherPic.src = "";
-  }
+  weatherPic.src = iconNameFunction(iconName);
 
   celsiusTemperature = response.data.main.temp;
 
